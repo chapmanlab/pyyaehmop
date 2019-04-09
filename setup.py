cb6f9a54ca749ad04dccffebe78adab65cc04c08
richardjgowers <richardjgowers@gmail.com>
2019-04-09 09:56:34 +0100

    make sure conda env include is included
    not sure why this is now needed

--- setup.py
+++ setup.py
@@ -1,7 +1,8 @@
 from setuptools import setup, find_packages
 from setuptools.extension import Extension
 from Cython.Build import cythonize
-from os import path
+from os import path, environ
+
 
 import numpy as np
 npgi = np.get_include()
@@ -10,12 +11,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), 'r') as f:
     long_descr = f.read()
 
+include_dirs = [npgi]
+# maybe in a conda env
+if 'CONDA_PREFIX' in environ:
+    include_dirs.append(path.join(environ['CONDA_PREFIX'], 'include'))
+
 pyeht = Extension(
     name="yaehmop",
     sources=["pyyaehmop/pyeht.pyx"],
     libraries=["yaehmop_eht", "lapack", "blas"],
     #library_dirs=["lib"],
-    include_dirs=[npgi],
+    include_dirs=include_dirs,
     extra_compile_args=['-ffast-math', '-O3'],
 )
 setup(
